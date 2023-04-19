0c0a5102c58feb9b7f8ad339bd3ed7eaf9c18f16
Aleksandr Trufmanov <kride2009@yandex.ru>
2023-04-19 19:10:52 +0300

    Code added

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,20 @@ public class Main {
         // сумма пополнения порог, и для этих двух разных
         // сценариев рассчитываете сумму бонуса и выводите
         // на экран.
+
+        int balance = 200;
+        int deposite = 1300;
+        int bonus;
+        if (deposite > 1000)
+        {
+            bonus = deposite / 100;
+        } else {
+            bonus = 0;
+        }
+        int newBalance = balance + deposite + bonus;
+        System.out.println("Начальный баланс=" + balance);
+        System.out.println("Сумма пополнения=" + deposite);
+        System.out.println("Бонус=" + bonus);
+        System.out.println("Новый баланс=" + newBalance);
+        }
     }
-}
